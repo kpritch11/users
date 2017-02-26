a77e589eb557212a1f63adfad1a51073b4243ba6
Kurtis Pritchard <kpritch11@gmail.com>
2017-02-25 18:23:12 -0600

    Added create_test

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-    .once('open', () => {
-        console.log('Mongoose connection started');
-    })
-    .on('error', (error) => {
-        console.warn('Warning', error);
+mongoose.Promise = global.Promise; // es6 promise
+
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test');
+    mongoose.connection
+        .once('open', () => {
+            console.log('Mongoose connection started');
+            done();
+        })
+        .on('error', (error) => {
+            console.warn('Warning', error);
+        });
+});
+
+beforeEach((done) => {
+    mongoose.connection.collections.users.drop(() => {
+        done();
     });
+});
