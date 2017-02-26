e53e30bb8c8c28882f1ba9bd713fe9589c34c3b4
Kurtis Pritchard <kpritch11@gmail.com>
2017-02-26 16:54:52 -0600

    Increment operator

--- src/user.js
+++ src/user.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise; // es6 promise
 
 const UserSchema = new Schema({
-    name: String
+    name: String,
+    postCount: Number
 });
 
 const User = mongoose.model('user', UserSchema);
--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating users', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({ name: 'Joe' });
+        joe = new User({ name: 'Joe', postCount: 0 });
         joe.save()
         .then(() => {
             done();
@@ -56,5 +56,14 @@ describe('Updating users', () => {
             User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
             done
         );
+    });
+
+    it('A user can have their post count incremented by 1', (done) => {
+        User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+        .then(() => User.findOne({ name: 'Joe' }))
+        .then((user) => {
+            assert(user.postCount === 1);
+            done();
+        });
     });
 });
