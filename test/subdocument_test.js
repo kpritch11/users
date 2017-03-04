2df1be4feda7950d0cab4525bdca3f8379cd8060
Kurtis Pritchard <kpritch11@gmail.com>
2017-03-04 08:38:56 -0600

    Add subdocuments to an existing record

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -25,4 +25,23 @@ describe('Subdocuments', () => {
             done();
         });
     });
+
+    it('can add subdocuments to an existing record', (done) => {
+        const joe = new User({
+            name: 'Joe',
+            posts: []
+        });
+
+        joe.save()
+        .then(() => User.findOne({ name: 'Joe' })) // .then(() => { return User.findOne({ name: 'Joe' }) })
+        .then((user) => {
+            user.posts.push({ title: 'New Post' });
+            return user.save()
+        })
+        .then(() => User.findOne({ name: 'Joe' }))
+        .then((user) => {
+            assert(user.posts[0].title === 'New Post');
+            done();
+        });
+    });
 });
