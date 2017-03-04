808f3b767c33925c95476e784b6ee018052ebd48
Kurtis Pritchard <kpritch11@gmail.com>
2017-03-04 11:31:39 -0600

    Added single association test

---
+++ test/association_test.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const assert = require('assert');
+const User = require('../src/user');
+const Comment = require('../src/comment');
+const BlogPost = require('../src/blog_post');
+
+describe('Associations', () => {
+    let joe, alex, blogPost, comment;
+
+    beforeEach((done) => {
+        // Create instances
+        joe = new User({ name: 'Joe' });
+        alex = new User({ name: 'Alex' });
+        blogPost = new BlogPost({
+            title: 'JS Rules',
+            content: 'Yes it does!'
+        });
+        comment = new Comment({ content: 'Congrats on the great post' });
+
+        // Assign associations
+        joe.blogPosts.push(blogPost);
+        blogPost.comments.push(comment);
+        comment.user = alex;
+
+        // Save instances, (these run in parallel not linearly)
+        Promise.all([
+            joe.save(),
+            alex.save(),
+            blogPost.save(),
+            comment.save()
+        ])
+        .then(() => { // once all promises are finished, call done()
+            done();
+        });
+    });
+
+    it('saves a relation between a user and a blogpost', (done) => {
+        User.findOne({ name: 'Joe' })
+        .populate('blogPosts')
+        .then((user) => {
+            assert(user.blogPosts[0].title === 'JS Rules');
+            done();
+        });
+    });
+});
